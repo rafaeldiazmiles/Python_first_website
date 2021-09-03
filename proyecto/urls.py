821f15a2fc905b712611545d9650c61d0c93851e
Rafael Diaz Miles <84687623+rafaeldiazmiles@users.noreply.github.com>
2021-09-03 17:47:10 -0300

    Add files via upload

---
+++ proyecto/urls.py
@@ -0,0 +1,37 @@
+"""proyecto URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls import handler404, handler500
+from django.conf.urls.static import static
+from bebidas.views import *
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', inicio, name='inicio'),
+    path('contacto', contacto, name='contacto'),
+    path('somos', somos, name='somos'),
+    path('buscar/', buscar, name='buscar'),
+    path('registro/', usuario_nuevo, name='registro'),
+    path('privado/', privado, name='privado'),
+    path('ingresar/', ingresar, name='ingresar'),
+    path('salir/', salir, name='salir'),
+    path('agregar/', agregar, name='agregar'),
+    path('agregar2/', agregar2, name='agregar2'),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
